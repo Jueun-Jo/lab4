10f7a3d9c85acf10b949465557fdef8914db6ade
jojueun <jooeun64@icloud.com>
2018-12-20 02:45:54 -0800

    guiProgramming

---
+++ ButtonWindow.cpp
@@ -0,0 +1,31 @@
+#include "ButtonWindow.h"
+#include <qpushbutton.h>
+#include <qapplication.h>
+#include <iostream>
+
+ButtonWindow::ButtonWindow(QWidget *parent, Qt::WindowFlags flags) : QMainWindow(parent, name)
+{
+	this->setWindowTitle("This is the window Title");
+	QPushButton *button = new QPushButton("Click Me!", this);
+	button->setGeometry(50,30,70,20);
+	connect (buttonb, SIGNAL(clicked()), this , SLOT(Clicked()));
+}
+
+ButtonWindow::~ButtonWindow()
+{
+}
+
+void ButtonWindow::Clicked(void)
+{
+	std::cout << "clicked!\n";
+}
+
+int main(int argc, char **argv)
+{
+	QApplication app(argc, argv);
+	ButtonWindow *window = new ButtonWindow();
+	
+
+	window->show();
+	return app.exec();
+}
---
+++ ButtonWindow.h
@@ -0,0 +1,15 @@
+#include <qmainwindow.h>
+
+
+
+class ButtonWindow : public QMainWindow
+{
+	Q_OBJECT
+	
+public:
+	ButtonWindow(QWidget *parentt = 0, Qt::WindowFlags flags = 0);
+	virtual ~ButtonWindow();
+	
+private slots:
+	void Clicked();
+};
